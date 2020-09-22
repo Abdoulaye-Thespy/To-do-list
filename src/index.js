375b7a78a180e6978ef25346c16ee787ae569935
Ubuntu <ubuntu@ip-172-31-24-139.us-east-2.compute.internal>
2020-09-22 13:52:52 +0000

    added function to render task dynnamically on the page

---
+++ src/index.js
@@ -0,0 +1,108 @@
+export let task = [{
+    title : "Razak",
+    description: "newttask",
+    dueDate: "12/12/12",
+    priority: "medium"
+},
+{
+    title : "Razak",
+    description: "newttask",
+    dueDate: "12/12/12",
+    priority: "medium"
+},
+{
+    title : "Razak",
+    description: "newttask",
+    dueDate: "12/12/12",
+    priority: "medium"
+},
+{
+    title : "Razak",
+    description: "newttask",
+    dueDate: "12/12/12",
+    priority: "medium"
+},
+{
+    title : "Razak",
+    description: "newttask",
+    dueDate: "12/12/12",
+    priority: "medium"
+},
+
+]
+ let cont=document.getElementById('conten');
+ const createButton = document.getElementById('create');
+ 
+ const Task = (title,description, dueDate, priority) => {
+    const getTittle = () => title;
+    const getDesciption = () => description;
+    const getdueDate = () => dueDate;
+    const getPriority = () => priority;
+    
+    return {getTittle, getDesciption, getdueDate, getPriority}
+     
+ }
+ 
+ createButton.onclick = function create () {
+  const title = document.getElementById('orangeForm-title').value;
+  const description = document.getElementById('orangeForm-description').value;
+  const dueDate = document.getElementById('orangeForm-date').value;
+  const priority = document.getElementById('orangeForm-priority').value;
+  const newTask = Task(title, description, dueDate, priority);
+  task.push(newTask);
+     console.log(newTask.getTittle)
+ }
+
+function render(title, description, dueDate, priority) {
+  const box = document.createElement('div');
+  box.className = 'col-md-3 task';
+  cont.appendChild(box);
+  const card = document.createElement('div');
+  card.className = 'card';
+  box.appendChild(card);
+  const cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+  card.appendChild(cardBody);
+  const cardTitle = document.createElement('h3');
+  cardTitle.className = 'card-title';
+  cardTitle.appendChild(document.createTextNode(title));
+  cardBody.appendChild(cardTitle);
+  const bookDescription = document.createElement('p');
+  bookDescription.className = 'card-text';
+  bookDescription.appendChild(document.createTextNode(`DUE DATE: ${dueDate}`));
+  cardBody.appendChild(bookDescription);
+  const numPage = document.createElement('h4');
+  numPage.className = 'card-title';
+  numPage.appendChild(document.createTextNode(` PRIORITY: ${priority}`));
+  cardBody.appendChild(numPage);
+  const del = document.createElement('a');
+  del.className = 'btn btn-danger bbd';
+  del.appendChild(document.createTextNode('DELETE'));
+  cardBody.appendChild(del);
+  const readOrNot = document.createElement('a');
+  readOrNot.className = 'btn btn-info';
+  readOrNot.appendChild(document.createTextNode('VIEW/EDIT'));
+  cardBody.appendChild(readOrNot);
+}
+let title="abdoulaye";
+let description="razak";
+let dueDate=11;
+let priority="read";
+
+for (let i = 0; i <task.length; i++) {
+  const { title } = task[i];
+  const { description } = task[i];
+  const { dueDate } = task[i];
+  const { priory } = task[i];
+  render(title, description, dueDate, priority);
+}
+
+// render(title, description, dueDate, priority);
+// render(title, description, dueDate, priority);
+// render(title, description, dueDate, priority);
+// render(title, description, dueDate, priority);
+// render(title, description, dueDate, priority);
+// render(title, description, dueDate, priority);
+// render(title, description, dueDate, priority);
+// render(title, description, dueDate, priority);
+// render(title, description, dueDate, priority);
